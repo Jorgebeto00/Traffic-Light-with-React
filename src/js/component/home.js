d844b7e8d2efd8bb48822557bb04734e54d30c08
Jorge R <jorgebeto00@protonmail.com>
2021-03-09 02:50:13 +0000

    final changes, complete

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,23 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 export function Home() {
-	// const [color, setColor] = useState("off");
-
-	// setColor(color){
-
-	// }
-
-	// useEffect(() => {
-
-	// });
+	let [colorRed, setColorR] = useState("off");
+	let [colorYellow, setColorY] = useState("off");
+	let [colorGreen, setColorG] = useState("off");
 
 	return (
 		<div className="container">
 			<div className="top"></div>
 			<div className="trafficLight">
-				<div className="round red"></div>
-				<div className="round yellow"></div>
-				<div className="round green"></div>
+				<div
+					className={"round " + colorRed}
+					onClick={() => {
+						setColorR((colorRed = "red-on"));
+						setColorG((colorGreen = "off"));
+						setColorY((colorYellow = "off"));
+					}}></div>
+				<div
+					className={"round " + colorYellow}
+					onClick={() => {
+						setColorY((colorYellow = "yellow-on"));
+						setColorR((colorRed = "off"));
+						setColorG((colorGreen = "off"));
+					}}></div>
+				<div
+					className={"round " + colorGreen}
+					onClick={() => {
+						setColorG((colorGreen = "green-on"));
+						setColorR((colorRed = "off"));
+						setColorY((colorYellow = "off"));
+					}}></div>
 			</div>
 		</div>
 	);
